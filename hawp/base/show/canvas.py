608ab89a161d0134ff11c22e3fccb2640ce70ff3
nico5655 <nicoukia.gamme@gmail.com>
2025-07-24 13:23:35 +0200

    correction

--- hawp/base/show/canvas.py
+++ hawp/base/show/canvas.py
@@ -92,6 +92,10 @@ class Canvas:
 
         if isinstance(image, str):
             image = cv2.imread(image)
+            if len(image.shape)==3 and image.shape[-1]==4:
+                image[image[...,-1]==0]=255
+                image=image[...,:-1]
+            image=image[...,::-1]
         else:
             image = np.asarray(image)
 
